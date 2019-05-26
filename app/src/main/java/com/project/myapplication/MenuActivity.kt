9c638d066ba42f728eb59a0fce3ff5b91346784c
Kamil <kamil.hrebeniuk@gmail.com>
2019-05-26 23:49:45 +0200

    Logging out

--- app/src/main/java/com/project/myapplication/MenuActivity.kt
+++ app/src/main/java/com/project/myapplication/MenuActivity.kt
@@ -1,25 +1,31 @@
 package com.project.myapplication
 
+import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 import com.project.myapplication.displayList.ProjectsListActivity
+
+private var preferences: SharedPreferences? = null
 
 class MenuActivity : AppCompatActivity() {
 
     private lateinit var requestQueue: RequestQueue
     private lateinit var connectionRequest: ConnectionRequest
+    private val PREFERENCESNAME = "CollabGamesSettings"
+    private val USERID = "UserID"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-
-
         requestQueue = Volley.newRequestQueue(this)
+        preferences = getSharedPreferences(PREFERENCESNAME, Activity.MODE_PRIVATE)
     }
 
     private fun prepareList() {
@@ -54,5 +60,12 @@ class MenuActivity : AppCompatActivity() {
 
     fun logOut(view: View) {
         Log.d("AAA", "Dziala")
+        val preferencesEditor = preferences!!.edit()
+        preferencesEditor.putInt(USERID, 0)
+        preferencesEditor.apply()
+        Toast.makeText(this@MenuActivity, "Logged Out", Toast.LENGTH_LONG).show()
+
+        
+        this.finishAffinity()
     }
 }
