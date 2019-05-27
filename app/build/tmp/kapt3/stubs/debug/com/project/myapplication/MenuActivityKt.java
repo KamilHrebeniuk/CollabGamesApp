cf2f83e4cc732349eb6592def652ceb73b54a08e
kasia <kasialebioda@gmail.com>
2019-05-27 16:19:02 +0200

    Missing string, resized and recolored lists items

--- app/build/tmp/kapt3/stubs/debug/com/project/myapplication/MenuActivityKt.java
+++
@@ -1,8 +0,0 @@
-package com.project.myapplication;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\b\n\u0000\n\u0002\u0018\u0002\n\u0000\"\u0010\u0010\u0000\u001a\u0004\u0018\u00010\u0001X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"preferences", "Landroid/content/SharedPreferences;", "app_debug"})
-public final class MenuActivityKt {
-    private static android.content.SharedPreferences preferences;
-}
